768b44967f920b9f5bd3b491a7672ed901174909
Manoj Shekhawat <manoj7shekhawat@gmail.com>
2022-08-21 10:22:23 +0200

    master - Added variables and provider file

--- azure/main.tf
+++ azure/main.tf
@@ -1,20 +1,4 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-
-  required_version = ">= 1.1.0"
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFrg"
-  location = "germanywestcentral"
+  name     = var.rg-name
+  location = var.location
 }
