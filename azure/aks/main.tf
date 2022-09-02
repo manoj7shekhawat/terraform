018893bba2e67684c4deadb73f2c7284bf94da94
Manoj Shekhawat <manoj7shekhawat@gmail.com>
2022-09-02 21:19:53 +0200

    master - added aks

--- azure/aks/main.tf
+++ azure/aks/main.tf
@@ -3,3 +3,22 @@ resource "azurerm_resource_group" "rg" {
   name     = var.rg-name
   location = var.location
 }
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = var.aks-name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.dns-prefix
+
+  default_node_pool {
+    name       = var.node-pool-name
+    node_count = 1
+    vm_size    = var.node-pool-size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = var.tags
+}
--- azure/aks/variables.tf
+++ azure/aks/variables.tf
@@ -5,3 +5,23 @@ variable "location" {
 variable "rg-name" {
   type = string
 }
+
+variable "aks-name" {
+  type = string
+}
+
+variable "dns-prefix" {
+  type = string
+}
+
+variable "node-pool-name" {
+  type = string
+}
+
+variable "node-pool-size" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
